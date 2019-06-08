abb07f9ce79e261a74f56fc75b38b2c537573d96
lihailong355 <lihailong355@pingan.com.cn>
2019-06-08 18:15:22 +0800

    Initial commit

---
+++ src/com/itcast/Hello.java
@@ -0,0 +1,23 @@
+package com.itcast;
+
+import com.report.html.HtmlFileGlobal;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        aa();
+    }
+
+
+    public static void aa() {
+        System.out.println("aaaaaa");
+        HtmlFileGlobal html = new HtmlFileGlobal();
+    }
+
+}
+
+
+
+
---
+++ src/com/itcast/web/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.itcast.web;
+
+import java.io.IOException;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.getWriter().write("hello servlet");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        doPost(request, response);
+    }
+
+}
+
